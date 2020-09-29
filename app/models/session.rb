4b891af487981e1a1667726a3654dc370afa028f
Maria Sitkovets <maria.sitkovets@shopify.com>
2020-09-29 10:32:09 -0400

    Remove associations

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 class Session < ActiveRecord::Base
   include ShopifyApp::ActualSessionStorage
-  self.primary_key = 'shopify_user_id'
-  has_one :user, foreign_key: "shopify_user_id"
   
   def api_version
     ShopifyApp.configuration.api_version
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 class User < ActiveRecord::Base
   include ShopifyApp::UserSessionStorage
-  self.primary_key = 'shopify_user_id'
-  has_many :sessions, foreign_key: "shopify_user_id"
   
   def api_version
     ShopifyApp.configuration.api_version
